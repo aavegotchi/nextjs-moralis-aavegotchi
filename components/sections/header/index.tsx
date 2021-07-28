e3d564f0958f7c5aacb36d3227754be6426c2f8f
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2021-07-28 21:45:40 +0100

    Gotchi tokens fetched

--- components/sections/header/index.tsx
+++ components/sections/header/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as Styled from "./styles";
+import { useMoralis } from 'react-moralis';
 import { ConnectButton } from 'components/ui';
 
 
 export const Header = () => {
+  const { Moralis, isAuthenticated, user } = useMoralis();
+
+  const getNFTs = async () => {
+    const options = { chain: 'matic', address: user.attributes.accounts[0] };
+    const polygonNFTs = await Moralis.Web3.getNFTs(options);
+    const ERC721TokenIds = polygonNFTs.reduce((currentArray, nft) => {
+      return nft.contract_type === "ERC721" ? [...currentArray, nft.token_id] : currentArray;
+    }, [])
+    console.log(ERC721TokenIds);
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getNFTs()
+    }
+
+  }, [isAuthenticated])
   return (
     <Styled.Wrapper>
       <ConnectButton />
